9b4a44443e8d523de37a01d4a4c679aeebf730ba
Cezary Babik <babik.czarek@gmail.com>
2025-12-22 20:00:28 +0100

    deploy prepare changes5

--- back/config/urls.py
+++ back/config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse  # <-- DODAJ TO
+
+def root(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("", root),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
 ]
